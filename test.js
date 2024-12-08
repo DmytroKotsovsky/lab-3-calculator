cb893bdf174ccd4c0753867ffe3a90a0e6818a57
DmytroKotsovsky <kocovskijdmitro@gmail.com>
2024-12-09 01:20:22 +0200

    wrong test result

--- test.js
+++ test.js
@@ -3,3 +3,7 @@ const { add } = require('./index');
 test('adds 5 + 5 to equal 10', () => {
     expect(add(5, 5)).toBe(10);
 });
+
+test('adds 5 + 6 to equal 10', () => {
+    expect(add(5, 5)).toBe(10);
+});
